895a2caa290a0f0ec39695beacfcdc4b0d5954cf
Zelig <qiyu2580@gmail.com>
2019-06-26 17:21:57 +0800

    fix: BUG

--- src/utils/toArea.js
+++ src/utils/toArea.js
@@ -3,9 +3,15 @@ import area from "./area.js";
 export default (code) => {
   if (!code) return
   code = code.toString();
-  // const province = code.slice(0, 2).padEnd(6, 0);
-  // const city = code.slice(0, 4).padEnd(6, 0);
+  const province = code.slice(0, 2).padEnd(6, 0);
+  const city = code.slice(0, 4).padEnd(6, 0);
   const county = code;
 
-  return area.county_list[county];
+  return (
+    area.province_list[province] +
+    "∙" +
+    area.city_list[city] +
+    "∙" +
+    area.county_list[county]
+  );
 }
