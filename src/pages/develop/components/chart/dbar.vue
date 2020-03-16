0ffe27be903ba0952dd07fedd4adea947dde142c
Zelig <qiyu2580@gmail.com>
2020-03-16 12:33:46 +0800

    feat: 新双条bar

--- src/pages/develop/components/chart/dbar.vue
+++ src/pages/develop/components/chart/dbar.vue
@@ -1,53 +1,112 @@
 <template>
-  <div>
-    <div class="flex align-center block" v-for="(v,i) in data" :key="i">
-      <div class="title">{{v.area}}</div>
-      <div class="process">
-        <process :width="v.value ? Math.ceil(v.value / vMax * 88) : 0" color="#686cff" :value="v.value" />
-        <process :width="v.value ? Math.ceil(v.num / nMax * 88) : 0" color="#e94c46" :value="v.num" />
-      </div>
-    </div>
-  </div>
+  <vebar :extend="chartExtend" :height="sale.length * 40 + 'px'" />
 </template>
 
 <script>
-import Process from "./process";
+import vebar from "v-charts/lib/bar.common";
 
 export default {
   props: {
-    name: String,
-    data: Array
+    y: Array,
+    attack: Array,
+    sale: Array,
+    tooltip: String
   },
 
   computed: {
-    vMax () {
-      let max = 0;
-      this.data.map(v => {
-        max = max < v.value ? v.value : max
-      })
+    maxs() {
+      let max1 = Math.max(...this.sale) * 1.2 || 10;
+      let max2 = Math.max(...this.attack) * 1.2 || 10;
+      let max = max1;
 
-      let len = Math.ceil(max).toString().length
-      let tmp = Math.pow(10, len - 1)
+      if (max1 < max2) max = max2
 
-      return Math.ceil(max / tmp) * tmp
+      const maxs = [];
+      for (let i = 0; i < this.sale.length; i++) {
+        maxs.push(max);
+      }
+
+      return maxs;
     },
-    nMax () {
-      let max = 0;
-      this.data.map(v => {
-        max = max < v.num ? v.num : max
-      })
+    chartExtend() {
+      return {
+        grid: {
+          left: 10,
+          right: 0,
+          top: 0,
+          bottom: 0
+        },
+        tooltip: { show: false },
+        xAxis: {
+          type: "value",
+          show: false,
+          axisLabel: {
+            show: false
+          },
+          max: this.maxs[0]
+        },
+        yAxis: {
+          type: "category",
+          data: this.y,
+          inverse: true
+        },
+        series: [
+          {
+            stack: "sale",
+            silent: true,
+            z: 3,
+            label: {
+              normal: {
+                show: true,
+                position: "right",
+                color: "black"
+              }
+            },
+            itemStyle: { color: "#686cff" },
+            type: "bar",
+            data: this.sale
+          },
+          {
+            type: "bar",
+            stack: "sale",
+            silent: true,
+            itemStyle: { color: "#f1f1f1" },
+            animation: false,
+            data: this.maxs,
+          },
 
-      let len = Math.ceil(max).toString().length
-      let tmp = Math.pow(10, len - 1)
-
-      return Math.ceil(max / tmp) * tmp
+          {
+            type: "bar",
+            silent: true,
+            z: 3,
+            stack: "attack",
+            itemStyle: { color: "#e94c46" },
+            label: {
+              normal: {
+                show: true,
+                position: "right",
+                color: "black"
+              }
+            },
+            data: this.attack
+          },
+          {
+            type: "bar",
+            stack: "attack",
+            silent: true,
+            itemStyle: { color: "#f1f1f1" },
+            animation: false,
+            data: this.maxs,
+          },
+        ]
+      };
     }
   },
 
   components: {
-    Process
+    vebar
   }
-}
+};
 </script>
 
 <style scoped>
@@ -60,11 +119,11 @@ export default {
   width: 100px;
   padding-left: 20px;
   height: auto;
-  overflow:hidden; 
-  text-overflow:ellipsis;
-  display:-webkit-box; 
-  -webkit-box-orient:vertical;
-  -webkit-line-clamp:2; 
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 2;
 }
 
 .process {
